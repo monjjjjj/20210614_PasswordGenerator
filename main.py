b8e6fbb78c75aa33dd3f4de2d2a6fb7c6d0d9baa
monjjjjj <monjjjj526@gmail.com>
2021-06-14 23:42:35 +0800

    Add comment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import string
 
+# define the length of password
 length = int(input("The length of the password you need:"))
 chars = string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation
 
