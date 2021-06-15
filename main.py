ff063905ddeb0c0b38138eb00300bdcbe8806e8e
monjjjjj <monjjjj526@gmail.com>
2021-06-15 14:53:01 +0800

    Check if input is a positive integer

--- main.py
+++ main.py
@@ -2,7 +2,17 @@ import random
 import string
 
 # define the length of password
-length = int(input("The length of the password you need:"))
+# length = int(input("The length of the password you need:"))
+while True :
+    length_input = input("The length of the password you need:")
+    if not length_input.isnumeric():
+        print(f"{length_input} is not a number, try again:")
+        continue
+    else :
+        length = int(length_input)
+        print(f"Password length: {length}")
+    break
+
 chars = string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation
 
 # use random
@@ -11,4 +21,4 @@ temp = random.sample(chars, length)
 # create password
 password = "".join(temp)
 
-print(password)
+print(f"Password: {password}")
