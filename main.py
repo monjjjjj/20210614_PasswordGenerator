cbde824002640d1021cd2924a82a8bcc45017f3d
monjjjjj <monjjjj526@gmail.com>
2021-06-16 01:08:04 +0800

    Add function

--- main.py
+++ main.py
@@ -1,62 +1,42 @@
 import random
 import string
-import numpy as np
+
+
+def get_length():
+    while True:
+        length_input = input("The length of the password you need:")
+        if not length_input.isnumeric():
+            print(f"{length_input} is not a number, try again:")
+        else:
+            return int(length_input)
+
+
+def check_source(source_name):
+    while True:
+        ans = input("%s, Y/N:" % source_name)
+        if ans == "Y":
+            return True
+        elif ans == "N":
+            return False
+        else:
+            print("Please enter Y/N!")
+
+
 
 # ========================================
 # define the length of password
-length = ""
-
-while True:
-    length_input = input("The length of the password you need:")
-    if not length_input.isnumeric():
-        print(f"{length_input} is not a number, try again:")
-        continue
-    else:
-        length = int(length_input)
-    break
+length = get_length()
 
 # ========================================
 # define source of the password
 # chars = string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation
-
-A = np.array([0, 0, 0, 0])
 chars2 = ""
 
-while True:
-    ans = input("Uppercase, Y/N:")
-    if ans == "Y" or ans == "N":
-        if ans == "Y":
-            chars2 += string.ascii_uppercase
-        break
-    else:
-        print("Please enter Y/N!")
-
-while True:
-    ans = input("Lowercase, Y/N:")
-    if ans == "Y" or ans == "N":
-        if ans == "Y":
-            chars2 += string.ascii_lowercase
-        break
-    else:
-        print("Please enter Y/N!")
-
-while True:
-    ans = input("Number, Y/N:")
-    if ans == "Y" or ans == "N":
-        if ans == "Y":
-            chars2 += string.digits
-        break
-    else:
-        print("Please enter Y/N!")
-
-while True:
-    ans = input("Symbol, Y/N:")
-    if ans == "Y" or ans == "N":
-        if ans == "Y":
-            chars2 += string.punctuation
-        break
-    else:
-        print("Please enter Y/N!")
+# 三元判斷式
+chars2 += string.ascii_uppercase if check_source("Uppercase") else ''
+chars2 += string.ascii_lowercase if check_source("Lowercase") else ''
+chars2 += string.digits if check_source("Number") else ''
+chars2 += string.punctuation if check_source("Symbol") else ''
 
 if chars2 == "":
     print("You need to choose one to say yes!")
