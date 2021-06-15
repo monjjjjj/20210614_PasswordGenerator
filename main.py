1d80e9b8f714c3dbe16f96ec1fa0340617e890e5
monjjjjj <monjjjj526@gmail.com>
2021-06-15 23:05:06 +0800

    Modify the defination of the source

--- main.py
+++ main.py
@@ -23,61 +23,45 @@ A = np.array([0, 0, 0, 0])
 chars2 = ""
 
 while True:
-    ans0 = input("Uppercase, Y/N:")
-    if ans0 == "Y":
-       chars2 += string.ascii_uppercase
-       break
-    elif ans0 == "N":
-       chars2 = chars2
-       break
+    ans = input("Uppercase, Y/N:")
+    if ans == "Y" or ans == "N":
+        if ans == "Y":
+            chars2 += string.ascii_uppercase
+        break
     else:
         print("Please enter Y/N!")
-    continue
-
 
 while True:
-    ans1 = input("Lowercase, Y/N:")
-    if ans1 == "Y":
-       chars2 += string.ascii_lowercase
-       break
-    elif ans1 == "N":
-       chars2 = chars2
-       break
+    ans = input("Lowercase, Y/N:")
+    if ans == "Y" or ans == "N":
+        if ans == "Y":
+            chars2 += string.ascii_lowercase
+        break
     else:
         print("Please enter Y/N!")
-    continue
 
 while True:
-    ans2 = input("Number, Y/N:")
-    if ans2 == "Y":
-       chars2 += string.digits
-       break
-    elif ans2 == "N":
-       chars2 = chars2
-       break
+    ans = input("Number, Y/N:")
+    if ans == "Y" or ans == "N":
+        if ans == "Y":
+            chars2 += string.digits
+        break
     else:
         print("Please enter Y/N!")
-    continue
 
 while True:
-    ans3 = input("Symbol, Y/N:")
-    if ans3 == "Y":
-       chars2 += string.punctuation
-       break
-    elif ans3 == "N":
-       chars2 = chars2
-       break
+    ans = input("Symbol, Y/N:")
+    if ans == "Y" or ans == "N":
+        if ans == "Y":
+            chars2 += string.punctuation
+        break
     else:
         print("Please enter Y/N!")
-    continue
 
 if chars2 == "":
     print("You need to choose one to say yes!")
 else:
-    chars1 = "".join(chars2)
-    temp = random.sample(chars1, length)
+    chars = "".join(chars2)
+    temp = random.sample(chars, length)
     password = "".join(temp)
     print(f"Password: {password}")
-
-
-
